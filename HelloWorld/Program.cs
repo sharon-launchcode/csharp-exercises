5683edde96a727b749a57fe6f864af536dac91c9
sharon-launchcode <sharon.launchcode@gmail.com>
2018-02-05 14:07:53 -0500

    input added

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Enter Your Name Here: ");
+                string userInput =  Console.ReadLine();
+                if (userInput != "")
+                {
+                Console.WriteLine("Hello,  " + userInput);
+                Console.ReadLine();
+                }
+                else
+                {
+                Console.WriteLine("Cannot be Empty");
+                Console.ReadLine();
+                }
+        }
+    }
+}
